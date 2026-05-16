8ac927651d0a05254ec56b421903665c05d06ea2
Jamila Omalan <jamilaomalan13@gmail.com>
2026-05-16 16:14:38 +0800

    fix: restore api export default

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // UPDATE THIS LINE EXACTLY:
-    baseURL: 'https://aclc-election-voting-system.onrender.com/api'
+    baseURL: 'https://aclc-election-voting-system.onrender.com/api' 
 });
 
-// Leave the rest of your interceptors/code exactly as they are
+// If you have interceptors for the token, they go here
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+// THIS IS THE LINE THAT IS MISSING/BROKEN:
+export default api;
